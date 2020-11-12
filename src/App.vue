0b7452c09ac86590424890a2f93d539770dab187
WANGXIAOFENG\36528 <xxlw_xiaofeng@163.com>
2020-11-12 10:05:31 +0800

    添加页面代码

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
-  <div id="app">
-    <h2>1111</h2>
+  <div id="app" class="wrapper">
+    <router-view/>
+    <main-tab-bar/>
   </div>
 </template>
-<script>
 
-export default {
-  name: 'App',
-  components: {
+<script>
+  import MainTabBar from 'components/content/mainTabbar/MainTabBar'
+
+  export default {
+    name: 'app',
+    components: {
+      MainTabBar
+    }
   }
-}
 </script>
+
 <style>
-  @import "./assets/css/base.css";
+  @import "assets/css/base.css";
 </style>
